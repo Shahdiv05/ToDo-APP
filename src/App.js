b22925b5a6a3cfa6cd1978a3f06d68d5dbec9067
Shahdiv05 <divyashah743@gmail.com>
2024-02-20 23:26:54 -0600

    Initial commit:

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import TodoList from './compnents/TodoList';
+
+
+//Stoped at 47.38
+
+function App() {
+  return (
+    <div className="todo-app">
+    <TodoList/>
+    </div>
+  );
+}
+
+export default App;
